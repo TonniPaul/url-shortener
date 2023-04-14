0dc3e5050e191d586745cc7884ef4f267cee4d87
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-04-14 12:18:15 +0100

    Saved links to local storage

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -5,20 +5,20 @@ import { SetStateAction, useState } from "react";
 import axios from "axios";
 
 interface LinksProp {
+  id: string;
   myUrl: string;
   shortenedUrl: string;
 }
-
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-
 const Input = () => {
   const [myUrl, setMyUrl] = useState("");
   const [myLinks, setMyLinks] = useState<LinksProp>({
+    id: "",
     myUrl: "",
     shortenedUrl: "",
   });
   const [errorMessage, setErrorMessage] = useState("");
   const [copied, setCopied] = useState<boolean>(false);
+  const [linksFromStorage, setLinksFromStorage] = useState<LinksProp[]>([]);
 
   const handleChange = (e: { target: { value: SetStateAction<string> } }) => {
     setMyUrl(e.target.value);
@@ -26,16 +26,26 @@ const Input = () => {
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
-    // Function to get data from API using the input value
     try {
       const response = await axios.post(
         `https://api.shrtco.de/v2/shorten?url=${myUrl}`
       );
-      setMyLinks({
+      const newLink = {
+        id: crypto.randomUUID(),
         myUrl: myUrl,
         shortenedUrl: response.data.result.short_link,
-      });
+      };
       setMyUrl("");
+
+      // Get the links array from local storage, or create an empty array if it doesn't exist yet
+      const links = JSON.parse(localStorage.getItem("links") || "[]");
+
+      // Add the new link to the links array and store it in local storage
+      const updatedLinks = [...links, newLink];
+      localStorage.setItem("links", JSON.stringify(updatedLinks));
+
+      // Update the links state with the new link
+      setLinksFromStorage(updatedLinks);
     } catch (error) {
       console.log(error);
     }
@@ -67,15 +77,17 @@ const Input = () => {
         <button>Shorten It!</button>
       </form>
 
-      {myLinks.shortenedUrl && (
-        <div className={inputStyle.myLinks}>
-          <p>{myLinks.myUrl}</p>
-          <p>{myLinks.shortenedUrl}</p>
-          <button onClick={handleCopy} id={`${copied && inputStyle.copied}`}>
-            {!copied ? "Copy" : "Copied!"}
-          </button>
-        </div>
-      )}
+      {linksFromStorage.reverse().map((data) => {
+        return (
+          <div key={data.id} className={inputStyle.myLinks}>
+            <p>{data.myUrl.toLowerCase()}</p>
+            <p>{data.shortenedUrl}</p>
+            <button onClick={handleCopy} id={`${copied && inputStyle.copied}`}>
+              {!copied ? "Copy" : "Copied!"}
+            </button>
+          </div>
+        );
+      })}
     </section>
   );
 };
